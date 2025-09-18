66b7d91f1454e44d3cbe2981a7ba32de33a013b6
Lars Nyström <nystrom.lasse@gmail.com>
2025-09-19 00:15:12 +0200

    refactor: change service lifetime from Scoped to Singleton for application services

--- src/FileHorizon.Application/ServiceCollectionExtensions.cs
+++ src/FileHorizon.Application/ServiceCollectionExtensions.cs
@@ -7,14 +7,14 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         // Core services
-        services.AddScoped<Core.IFileProcessingService, Core.FileProcessingService>();
+        services.AddSingleton<Core.IFileProcessingService, Core.FileProcessingService>();
         // Infrastructure defaults
-        services.AddScoped<Abstractions.IFileProcessor, Infrastructure.FileProcessing.NoOpFileProcessor>();
+        services.AddSingleton<Abstractions.IFileProcessor, Infrastructure.FileProcessing.NoOpFileProcessor>();
         services.AddSingleton<Abstractions.IFileEventQueue, Infrastructure.Queue.InMemoryFileEventQueue>();
         // Pollers: register concrete implementations then selector facade
-        services.AddScoped<Infrastructure.Polling.SyntheticFilePoller>();
-        services.AddScoped<Infrastructure.Polling.LocalDirectoryPoller>();
-        services.AddScoped<Abstractions.IFilePoller, Infrastructure.Polling.PollerSelector>();
+        services.AddSingleton<Infrastructure.Polling.SyntheticFilePoller>();
+        services.AddSingleton<Infrastructure.Polling.LocalDirectoryPoller>();
+        services.AddSingleton<Abstractions.IFilePoller, Infrastructure.Polling.PollerSelector>();
         services.AddSingleton<Abstractions.IFileEventValidator, Validation.BasicFileEventValidator>();
         return services;
     }
