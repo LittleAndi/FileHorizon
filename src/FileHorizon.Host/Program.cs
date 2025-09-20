18dff5272a6e4e7606a30fe1d7682e1ff7514a74
Lars Nyström <nystrom.lasse@gmail.com>
2025-09-20 14:46:44 +0200

    feat: configure RedisOptions in the application services

--- src/FileHorizon.Host/Program.cs
+++ src/FileHorizon.Host/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddApplicationServices();
 builder.Services.Configure<PollingOptions>(builder.Configuration.GetSection(PollingOptions.SectionName));
 builder.Services.Configure<FileSourcesOptions>(builder.Configuration.GetSection(FileSourcesOptions.SectionName));
 builder.Services.Configure<PipelineFeaturesOptions>(builder.Configuration.GetSection(PipelineFeaturesOptions.SectionName));
+builder.Services.Configure<RedisOptions>(builder.Configuration.GetSection(RedisOptions.SectionName));
 builder.Services.AddHostedService<FilePipelineBackgroundService>();
 builder.Services.AddHealthChecks();
 
